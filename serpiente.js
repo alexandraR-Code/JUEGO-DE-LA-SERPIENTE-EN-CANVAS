2b2832785f7a8bcb8b24ff0898189c29a5882ba4
Alexandra Ramirez <alexa090114@gmail.com>
2026-05-10 19:45:37 -0500

    Agregar funcion dibujarTablero con lineas verticales y horizontales usando for

--- serpiente.js
+++ serpiente.js
@@ -1,25 +1,64 @@
 
-    // 1. Capturamos el canvas y su contexto de dibujo
-    const canvas = document.getElementById("canvasJuego");
-    const ctx = canvas.getContext("2d");
+ // 1. Capturamos el canvas y su contexto de dibujo
+const canvas = document.getElementById("canvasJuego");
+const ctx = canvas.getContext("2d");
+  
 
 
-    
 
-    // Primera pintura del juego al cargar la página
-    dibujarTodo();
+const TAMANIO_CELDA = 25;
 
-    // =========================
-    // FUNCIONES DE DIBUJO
-    // =========================
+  // Primera pintura del juego al cargar la página
+dibujarTodo();
 
-    function limpiarCanvas() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
+// =========================
+// FUNCIONES DE DIBUJO
+// =========================
 
-    function dibujarTodo() {
-      limpiarCanvas();
-    }
+function limpiarCanvas() {
+  ctx.fillStyle = "green";
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function dibujarTodo() {
+  limpiarCanvas();
+  dibujarTablero()
+}
+
+function dibujarTablero(){
+  ctx.strokeStyle = "blue"; // define el color de la linea
+  ctx.beginPath(); //Le dice al lapiz voy a empezar a dibujar la linea
+  ctx.moveTo(25,0); //Levanta el lapiz y lo pone en la poscion 0,0
+  ctx.lineTo(25,600); //Traza una linea hasta el centro del canvas
+  ctx.stroke(); //Finaliza dibuja la linea
+  ////
+  ctx.strokeStyle = "blue"; // define el color de la linea
+  ctx.beginPath(); //Le dice al lapiz voy a empezar a dibujar la linea
+  ctx.moveTo(50,0); //Levanta el lapiz y lo pone en la poscion 0,0
+  ctx.lineTo(50,600); //Traza una linea hasta el centro del canvas
+  ctx.stroke(); //Finaliza dibuja la linea
+  ////
+  ctx.strokeStyle = "blue"; // define el color de la linea
+  ctx.beginPath(); //Le dice al lapiz voy a empezar a dibujar la linea
+  ctx.moveTo(75,0); //Levanta el lapiz y lo pone en la poscion 0,0
+  ctx.lineTo(75,600); //Traza una linea hasta el centro del canvas
+  ctx.stroke(); 
+  
+  for(let i=0; i <= canvas.width; i+=TAMANIO_CELDA){
+    ctx.strokeStyle = "blue"; // define el color de la linea
+    ctx.beginPath(); //Le dice al lapiz voy a empezar a dibujar la linea
+    ctx.moveTo(i,0); //Levanta el lapiz y lo pone en la poscion 0,0
+    ctx.lineTo(i, canvas.height); //Traza una linea hasta el centro del canvas
+    ctx.stroke();
+  }
+  for(let i=0; i<=canvas.height; i+=TAMANIO_CELDA){
+    ctx.strokeStyle = "blue";
+    ctx.beginPath();
+    ctx.moveTo(0,i);
+    ctx.lineTo(canvas.width,i);
+    ctx.stroke();
+  }
+}
 
 
 

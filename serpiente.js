74b55f8a6cbafc3f1071f58df718fc9199848fdf
Alexandra Ramirez <alexa090114@gmail.com>
2026-05-14 21:18:37 -0500

    feat: implementar game over, reiniciar juego, velocidad y evitar retroceso

--- serpiente.js
+++ serpiente.js
@@ -14,22 +14,14 @@ const serpiente = [
   {LINEASX: 15, LINEASY:16},
   {LINEASX: 16, LINEASY:16},
   {LINEASX: 17, LINEASY:16},
-  {LINEASX: 18, LINEASY:16},
-  {LINEASX: 19, LINEASY:16},
-  {LINEASX: 19, LINEASY:17},
-  {LINEASX: 19, LINEASY:18},
-  {LINEASX: 19, LINEASY:19},
-  {LINEASX: 18, LINEASY:19},
-  {LINEASX: 18, LINEASY:19},
-  {LINEASX: 17, LINEASY:19},
-  {LINEASX: 16, LINEASY:19},
-
+  {LINEASX: 18, LINEASY:16}
 ];
 let intervaloSerpiente; // variable que almacena el intervalo del movimiento  de la serpiente 
 let direccionActual = "derecha"; // Guarada la ultima direccion seleccionada 
 let comidaX;
 let comidaY;
 let puntaje = 0;
+let velocidad = 300; //velocidad aunmentara a medida que avance de nivel 
 
 
   // Primera pintura del juego al cargar la página
@@ -49,7 +41,6 @@ function dibujarTodo() {
   dibujarTablero();
   pintarSerpiente();
   pintarComida();
-  
 }
 
 function dibujarTablero(){
@@ -114,13 +105,24 @@ function moverDerecha(){
   serpiente.unshift(nuevaCabeza); //Agregamos la nueva cabeza al inicio del arreglo
   serpiente.pop();                //Eliminamos la ultima parte de la serpiente 
 }
-function cambiarDireccion(direccion){
-  //"direccion" puede llegar como derecha, arriba, abajo, izquierda
-  //Indicamos cual llego para saber cual es la correcta
-  //if(direccionActual === 'derecha'){
-   // moverDerecha(); // solo se llama si llego la derecha
-   // Actualiza la direccion actual para evitar movimientos invalidos 
-  direccionActual = direccion; 
+function cambiarDireccion(direccion){ 
+  // Si la serpiente va a la derecha, no puede ir a la izquierda
+  if(direccionActual === 'derecha' && direccion === 'izquierda'){
+    return;// detiene la función e ignora el cambio de dirección
+  }
+  // Si la serpiente va a la izquierda, no puede ir a la derecha
+  if(direccionActual === 'izquierda' && direccion === 'derecha'){
+    return;// detiene la función e ignora el cambio de dirección
+  }
+  // Si la serpiente va para arriba, no puede ir para abajo
+  if(direccionActual === 'arriba' && direccion === 'abajo'){
+    return;// detiene la función e ignora el cambio de dirección
+  }
+  // Si la serpiente va para abajo, no puede ir para arriba
+  if(direccionActual === 'abajo' && direccion === 'arriba'){
+    return;// detiene la función e ignora el cambio de dirección
+  }
+  direccionActual = direccion;
 }
 
 function moverIzquierda(){
@@ -148,7 +150,10 @@ function moverAbajo(){
   serpiente.pop();
 }
 function iniciarJuego(){
-  intervaloSerpiente = setInterval(moverSerpiente, 1000); 
+  // Inicia el movimiento automático de la serpiente ejecutando 
+  // moverSerpiente() cada cierto tiempo según la variable velocidad
+  intervaloSerpiente = setInterval(moverSerpiente, velocidad);
+  generarComida(); 
 }
 function pausarJuego(){
   // detiene el intervalos del movimieto de la serpiente 
@@ -161,26 +166,35 @@ function moverSerpiente(){
   }else if(direccionActual === 'arriba'){moverArriba();
   }else if(direccionActual === 'abajo'){moverAbajo(); 
   }
+  //valida que el juego termine si la cabeza de la serpiente esta fuera del lienzo 
+  if(serpiente[0].LINEASX < 0 || serpiente[0].LINEASX >= 24 || serpiente[0].LINEASY < 0 || serpiente[0].LINEASY >= 24){
+    pausarJuego();
+    document.getElementById("mensaje").innerText = "Juego terminado - reinicia para jugar de nuevo";
+    //Detiene el juego 
+    return; 
+  }
   //redibuja la serpiente en una nueva posicion 
-  pintarSerpiente();
-  
+  dibujarTodo();
   //Verifica si la cabeza d ela serpente toco la comida 
   if(atraparComida() == true){
     //Si atrapo la comida, suma 1  al puntaje
     puntaje++;
+    generarComida();
      //Si atrapo la comida la serpiente crece, agregamos un nuevo segmento al final
     serpiente.push(serpiente[serpiente.length - 1]);
   }
   //Actualiza el puntaje en el HTML 
-  document.getElementById("puntaje").innerText = puntaje;  
+  document.getElementById("puntaje").innerText = puntaje; 
 }
 function pintarComida(){
+  // pintamos la comida en el canvas con diferencia de color
+  pintarParte(comidaX, comidaY, "orange");
+}
+function generarComida(){
   // Generar coodernaas aeleatorias para la comida
   comidaX = Math.floor(Math.random() * canvas.width / TAMANIO_CELDA);
   comidaY = Math.floor(Math.random() * canvas.height / TAMANIO_CELDA);
   
-  // pintamos la comida en el canvas con diferencia de color
-  pintarParte(comidaX, comidaY, "orange");
 }
 function atraparComida(){
   // verificar si la cabeza de la serpiente toca la comida 
@@ -193,6 +207,31 @@ function atraparComida(){
     console.log("Comida no atrapada");
     return false;
   }
-  
-
+}
+function reiniciarJuego(){
+  //limpiar el tablero 
+  limpiarCanvas();
+  //Esto elimina todos los elementos del arreglo sin reasignarlos
+  serpiente.length = 0;
+  //push agrega nuevos elementos al arreglo, en este caso en la mismas posicion inicial
+  serpiente.push(
+      {LINEASX: 15, LINEASY:15},
+      {LINEASX: 15, LINEASY:16},
+      {LINEASX: 16, LINEASY:16},
+      {LINEASX: 17, LINEASY:16},
+      {LINEASX: 18, LINEASY:16}
+  );
+  //Reinicia la direccion a la derecha 
+  direccionActual = "derecha";
+  //Reinicia el puntaje a 0
+  puntaje = 0;
+  //Actualiza el puntae en el HTML
+  document.getElementById("puntaje").innerText = puntaje;
+  //Detenemos el juego si estaba en curso
+  pausarJuego();
+  //Inicia el juego de nuevo
+  iniciarJuego();
+  //Limpia el mensaje de juego terminado 
+  document.getElementById("mensaje").innerText = "";
+  dibujarTodo();
 }
